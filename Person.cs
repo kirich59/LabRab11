eccb00b3d66c4f69f409031348be3a3224390ef1
LocalUser <kirillbazhenov45@gmail.com>
2018-04-17 14:35:03 +0500

    my first commit

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRab11
+{
+    class Person
+    {
+        protected string name;
+        protected string secondName;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string SecondName
+        {
+            get { return secondName; }
+            set { secondName = value; }
+        }
+        public Person()
+        {
+            name = "";
+            secondName = "";
+        }
+        public Person(string Name, string SecondName)
+        {
+            name = Name;
+            secondName = SecondName;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Имя: " + name + "\nФамилия: " + secondName);
+        }
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRab11
+{
+    class Student:Person
+    {
+        protected byte degree;    //курс
+        protected double average; //средний балл за сессию
+        public byte Degree
+        {
+            get { return degree; }
+            set { degree = value; }
+        }
+        public double Average
+        {
+            get { return average; }
+            set { average = value; }
+        }
+        public Student() : base()
+        {
+            degree = 0;
+            average = 0;
+        }
+        public Student(string Name, string SecondName,byte Degree,double Average) : base(Name, SecondName)
+        {
+            degree = Degree;
+            average = Average;
+        }
+    }
+}
